4fd3e082d6063880c2dd781f82a39bc3326da361
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-07-15 10:25:40 +0100

    Implement mod enabled toggle

--- Installers/PlayerInstaller.cs
+++ Installers/PlayerInstaller.cs
@@ -7,6 +7,11 @@ internal class PlayerInstaller : Installer
 {
     public override void InstallBindings()
     {
+        if (!Container.Resolve<PluginConfig>().Enabled)
+        {
+            return;
+        }
+
         Container.BindInterfacesAndSelfTo<SiraSaberFetcher>().AsSingle();
         Container.BindInterfacesTo<SaberColorProcessor>().AsSingle();
         Container.BindInterfacesTo<SaberColorer>().AsSingle();
